2fb978d2b018f2fc494de005da3f2be2cfc7bec0
ChrisZengqin <Zq136170517@>
2021-07-21 15:57:31 +0800

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @Author： Chris 曾勤
+ * @CreateDate： 2021/7/21 15:51
+ * @Version： 1.0.0
+ * @Description：
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+}
